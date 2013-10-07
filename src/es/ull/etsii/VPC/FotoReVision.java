22b84dcd2c91a16b49fe0e6ca1b20dc39a09b1b7
Nesvi <nesvikiller@gmail.com>
2013-10-07 22:22:23 +0100

    I have selected the chart4j library for the charts. I have included it and I coded a test method of PhotoVision class named chartTest()

--- src/es/ull/etsii/VPC/FotoReVision.java
+++ src/es/ull/etsii/VPC/FotoReVision.java
@@ -4,12 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import com.googlecode.charts4j.Data;
+import com.googlecode.charts4j.GCharts;
+import com.googlecode.charts4j.LineChart;
+import com.googlecode.charts4j.Plot;
+import com.googlecode.charts4j.Plots;
 
 public class FotoReVision extends JFrame{
 
@@ -35,7 +46,8 @@ public class FotoReVision extends JFrame{
       setTitle( "FotoReVisión" );
       setLocationRelativeTo ( null );
       setVisible(true);
-
+      
+      chartTest ();
    }
    
    public void initializeComponents(){
@@ -73,6 +85,28 @@ public class FotoReVision extends JFrame{
       out.setTitle( "FotoReVisión" );
       out.setLocationRelativeTo ( null );
       out.setVisible(true);
+   }
+   
+   public void chartTest(){
+      Plot plot = Plots.newPlot(Data.newData(0, 66.6, 33.3, 100));
+      LineChart chart = GCharts.newLineChart(plot);
+      
+      JFrame frame = new JFrame();
+      JLabel label = null;
+      
+      try {
+	 label = new JLabel(new ImageIcon(ImageIO.read(new URL(chart.toURLString ()))));
+	 System.out.println (chart.toURLString ());
+      } catch (MalformedURLException e) {
+	 e.printStackTrace();
+      } catch (IOException e) {
+	 e.printStackTrace();
+      }
+      
+      frame.getContentPane().add(label, BorderLayout.CENTER);
+      frame.pack();
+      frame.setVisible(true);
+   
    }
 
 }
